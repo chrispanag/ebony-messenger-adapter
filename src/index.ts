5313cd15b67f86a73b1f85e76a096a4142684b42
Christos Panagiotakopoulos <chrispanag@gmail.com>
2019-09-25 14:35:00 +0300

    Quality of Code changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as sendAPI from './sendAPI';
 import MessengerAdapter from './adapter/adapter';
+import MessengerUser from './adapter/MessengerUser';
 
 export {
     sendAPI,
-    MessengerAdapter
+    MessengerAdapter,
+    MessengerUser
 }
